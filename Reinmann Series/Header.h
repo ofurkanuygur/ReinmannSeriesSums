f12dd5c3b848c1ce46fb8b01846778f618b541e6
Oktay Furkan Uygur <ofurkanuygur@gmail.com>
2017-03-01 11:47:19 +0200

    Reinmann

---
+++ Reinmann Series/Header.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+//3.dereceden bir denklemin katsayılarını reinmann toplamınında kullanılacak aralıkları ve aralıkların kaç eşit parçaya bölünecegini bulucaz
+//reinman toplamının sonucunu bulucak bir program..
+
+using namespace std;
+
+struct equation
+{
+	double a, b, c, d;
+};
+
+void print_function(struct equation *d);
+double calculate_function(struct equation *d, double x);
+double calculate_reinmann(struct equation *d, double a, double b, int n);
+
+
+
+void print_function(struct equation *d) {
+
+	cout << "y = " << d->a << "x^3";
+	cout << (d->b < 0 ? " - " : " + ") << abs(d->b) << "x^2";
+	cout << (d->c < 0 ? " - " : " + ") << abs(d->c) << "x";
+	cout << (d->d < 0 ? " - " : " + ") << abs(d->d) <<endl;
+}
+
+
+double calculate_function(struct equation *d, double x) {
+
+	return d->a*x*x*x + d->b*x*x + d->c*x + d->d;
+
+}
+
+
+double calculate_reinmann(struct equation *d, double a, double b, int n) {
+
+	double x, y, toplam = 0, dx = (b - a) / n;
+	for (int i = 0; i < n; i++)
+	{
+		x = a + i*dx + dx / 2;
+		y = calculate_function(d, x);
+		toplam += y*dx;
+	}
+	return toplam;
+}
---
+++ Reinmann Series/Source.cpp
@@ -0,0 +1,25 @@
+﻿#include"Header.h"
+
+int main() {
+
+	struct equation f;
+	double initial_value, last_value;
+	int rectangle;
+
+	cout << fixed << setprecision(1);// virgülden soranki basamak sayisinini 1 olarak ayarladık..
+
+	cout << "Enter the coefficients of the equation :  " << endl;
+	cin >> f.a >> f.b >> f.c >> f.d;//fonksiyonu aldım
+
+	cout << "Equation Entered : " << endl;
+	print_function(&f); //fonksiyonu yaz..
+
+	cout << "Enter range values and section number :" << endl;
+	cin >> initial_value >> last_value >> rectangle; // aralık değerlerini aldım..
+
+	cout << "Reinmann Sums : ";
+	cout << calculate_reinmann(&f, initial_value, last_value, rectangle);
+	cout << endl;
+	system("pause");
+	return 0;
+}
